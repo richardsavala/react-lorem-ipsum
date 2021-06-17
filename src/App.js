c1e8dea32b2467f8635e16b6636b9d843c771dbd
Richard <82630326+richardsavala@users.noreply.github.com>
2021-06-16 20:00:12 -0600

    Add files via upload
    structure
    in App.js added variables count, text, and handleSubmit 
    in App.js added section, form and submit button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hell world");
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit"></button>
+      </form>
+    </section>
+  );
 }
 
 export default App;
